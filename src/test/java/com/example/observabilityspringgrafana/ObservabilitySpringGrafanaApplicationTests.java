6dcced1e162ddd0c9521db223b90cc3612becb8e
OUAQAROUADIE <117985234+OUAQAROUADIE@users.noreply.github.com>
2024-09-05 15:06:32 +0100

    commit

--- src/test/java/com/example/observabilityspringgrafana/ObservabilitySpringGrafanaApplicationTests.java
+++ src/test/java/com/example/observabilityspringgrafana/ObservabilitySpringGrafanaApplicationTests.java
@@ -3,11 +3,8 @@ package com.example.observabilityspringgrafana;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+//@SpringBootTest
 class ObservabilitySpringGrafanaApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
 
 }
